8212b335923734dda86a13ac9dc76f9ff69c605f
Saheen Shoukath <saheenshoukath@gmail.com>
2022-08-04 19:27:03 +0530

    feat: add tailwindcss

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`bg-red-500 h-full`}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
